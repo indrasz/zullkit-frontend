d30f47f9cedd9a3d7029b4b1568a01ad995721d4
wahyuindra <wahyuindra712@gmail.com>
2023-05-23 11:19:02 +0700

    first commit

---
+++ src/components/homepage/Categories.vue
@@ -0,0 +1,16 @@
+
+
+<template>
+    <div class="container px-4 mx-auto my-16 md:px-12" id="categories">
+        <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">Top Categories</h2>
+        <div class="flex flex-wrap -mx-1 lg:-mx-4">
+            <CategoryCard 
+                v-for="category in categories" 
+                :id="category.id" 
+                :key="category.id" 
+                :title="category.name" 
+                :count="category.products_count"
+                :image="category.thumbnails" />
+        </div>
+    </div>
+</template>
---
+++ src/components/homepage/Items.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container px-4 mx-auto my-16 md:px-12">
+        <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">New Items</h2>
+        <div class="flex flex-wrap -mx-1 lg:-mx-4">
+            <ItemCard 
+                v-for="item in items" 
+                :key="item.id" 
+                :id="item.id"
+                :title="item.name" 
+                :description="item.subtitle"
+                :image="item.thumbnails" />
+        </div>
+    </div>
+</template>
